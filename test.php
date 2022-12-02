ef87f4431347efafed89c0721503f38a11205c92
Oluwaniayomi Ogundana <oluwaniayomi.ogundana@student.manchester.ac.uk>
2022-12-02 00:52:58 +0000

    Test file created.

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Just Testing</title>
+</head>
+<body>
+	This is the test website for instruction 15 of Section 4. COMP10120 lab for git.
+</body>
+</html>
