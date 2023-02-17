428d789d2d91d066b1501179119d063667f55fa1
Oluwaniayomi Ogundana <oluwaniayomi.ogundana@student.manchester.ac.uk>
2023-02-17 15:11:16 +0000

    Reaction test javascript file has now been commented.

--- reactionTest.js
+++ reactionTest.js
@@ -9,25 +9,17 @@ let lights = ["lab1", "lab2", "lab3", "lab4", "lab5"];
 let li_index = 0
 let first = true;
 
-// while (running){
-// 	console.log("started")
-// 	// slightPause()
-// 	// currentTime = currentTime + 1;
-// 	document.getElementById("timer").innerHTML = 0.01
-// 	console.log("finished")
-// }
-
 function startGame(){
-    // for (let i=0; i<lights.length; i++){
-    //     console.log(i)
-    //     lightTimer(lights[i]);
-    // }
+    // When start button is clicked, game is set to its original state
+    // lightTimer is then called so the lights start going green
     resetGame();
     console.log(li_index)
     lightTimer(lights[li_index]);
 }
 
 function lightTimer(current_id){
+    // This causes the delay between the lights changing.
+    // Each light changes after a random amount of time under 10s
     if (lightsGreen == false){
         console.log("Here");
         light_interval = setInterval(changeLight, Math.floor(Math.random() * 1000))
@@ -35,6 +27,8 @@ function lightTimer(current_id){
 }
 
 function changeLight(){
+    // This waits the first time the function is called but changes a light the second time.
+    // This is done to allow for the interval betwen lights changing to take place
     let current_id=lights[li_index];
     console.log("Starting here");
     if (first){
@@ -50,6 +44,7 @@ function changeLight(){
             lightsGreen = true;
             li_index = 0;
             clearInterval(light_interval);
+            // After the last light changes, the timer for the reaction starts
             interBegin();
         }
         clearInterval(light_interval);
@@ -59,9 +54,11 @@ function changeLight(){
 }
 
 function stopGame(){
+    // When reaction button is clicked, timer stops if all lights have gone green
     if (lightsGreen==true){
         stopTimer();
     }
+    // If the lights haven't all changed, then the button goes red to show they went too early
     else{
         document.getElementById("clickButton").style.cssText = "background-color: red";
         clearInterval(light_interval);
@@ -69,12 +66,14 @@ function stopGame(){
 }
 
 function interBegin(){
+    // This increases the timer by 10 milliseconds every 10 milliseconds
 	if (running == false){
 		interval = setInterval(startTimer, 10)
 	}
 }
 
 function resetGame(){
+    // This changes the background colour back to the original and calls resetTimer
     document.getElementById("clickButton").style.cssText = "background-color: blanchedalmond";
     for (let i = 0; i < lights.length; i++){
         document.getElementById(lights[i]).style.cssText = "background-color: red";
@@ -86,6 +85,7 @@ function resetGame(){
 }
 
 function startTimer(){
+    // This controls the timer and how it is displayed on the screen
 	running = true;
 	console.log("working...")
 	console.log(time)
@@ -113,12 +113,14 @@ function startTimer(){
 
 
 function stopTimer(){
+    // This stops the timer from running
 	console.log("stop clicked")
 	clearInterval(interval)
 	running = false
 }
 
 function resetTimer(){
+    // This sets the timer being shown back to 00.00 and stops it if it is still going
 	console.log("Reset clicked")
 	clearInterval(interval)
 	time = 0
