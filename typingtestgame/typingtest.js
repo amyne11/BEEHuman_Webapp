16aeaa9435527c11c992da54b5208e37089615cf
h42621az <allen.zhao@student.manchester.ac.uk>
2023-02-17 14:03:30 +0000

    Made progress on typing test game

---
+++ typingtestgame/typingtest.js
@@ -0,0 +1,49 @@
+let text = "The quick brown fox jumps over the lazy dog";
+let currentindex = 0;
+let pOffset = 0;
+
+let typingtext = document.createElement("p");
+typingtext.innerHTML = text;
+
+document.getElementById("typingtext").appendChild(typingtext);
+
+
+document.onkeydown = function (e) {
+    e = e || window.event;
+
+    if(e.shiftKey) return;
+
+    if (e.key == text[currentindex])
+    {
+        currentindex++;
+    }
+    else
+    {
+
+        //The beginning of the red span of text
+        let firstP = "<span style='color:red'>";
+        let closingP = "</span>";
+        
+        //Find where we are in the inner html taking into account spans we have added
+        let indexWithOffset = pOffset + currentindex;
+
+        //Find left of the string with regard to our offset index
+        let leftSpan = text.substring(0,indexWithOffset);
+        //Find character we are currently on
+        let letter = text[indexWithOffset];
+        //Find right of the string with regard to our offset index
+        let rightSpan = text.substring(indexWithOffset + 1, text.length - 1);
+
+        //construct innerHtml by, [LEFT OF STRING] + <span style='color:red'> + [LETTER] + </span> + [RIGHT OF STRING]
+        text = leftSpan + firstP + letter + closingP + rightSpan;
+
+        //apply changes to html
+        typingtext.innerHTML = text;
+
+        //Add our offset by adding both lengths of our span tags
+        pOffset += firstP.length + closingP.length;
+        currentindex++;
+
+        alert(text);
+    }
+};
