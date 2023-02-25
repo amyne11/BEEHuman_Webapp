c6eb171fa51c93d57e4bfe35511925e7db37f342
Allen <allen.zhao@student.manchester.ac.uk>
2023-02-25 00:48:58 +0000

    Fixed bugs, now it'll glow red with a wrong letter and green with a correct one

--- typingtestgame/typingtest.js
+++ typingtestgame/typingtest.js
@@ -11,7 +11,9 @@ document.getElementById("typingtext").appendChild(typingtext);
 document.onkeydown = function (e) {
     e = e || window.event;
 
-    if(e.shiftKey) return;
+    if (e.shiftKey || e.ctrlKey) return;
+    
+    
 
     let closingP = "</span>";
 
@@ -25,44 +27,32 @@ document.onkeydown = function (e) {
     //Find right of the string with regard to our offset index
     let rightSpan = text.substring(indexWithOffset + 1, text.length);
 
-
-    if (e.key == text[currentindex])
-    {
-        //The beginning of the red span of text
+    if (letter === e.key) {
+        //The beginning of the green span of text
         let firstP = "<span style='color:green'>";
         
-        
-        
-
-        //construct innerHtml by, [LEFT OF STRING] + <span style='color:green'> + [LETTER] + </span> + [RIGHT OF STRING]
+        //Construct innerHtml by, [LEFT OF STRING] + <span style='color:green'> + [LETTER] + </span> + [RIGHT OF STRING]
         text = leftSpan + firstP + letter + closingP + rightSpan;
 
-        //apply changes to html
-        typingtext.innerHTML = text;
-
-        //Add our offset by adding both lengths of our span tags
-        pOffset += firstP.length + closingP.length;
-        currentindex += firstP.length + 1;
-
-        alert(currentindex)
-    }
-    else
-    {
-
-        //The beginning of the red span of text
-        let firstP = "<span style='color:red'>";
-        
-
-        //construct innerHtml by, [LEFT OF STRING] + <span style='color:red'> + [LETTER] + </span> + [RIGHT OF STRING]
-        text = leftSpan + firstP + letter + closingP + rightSpan;
-
-        //apply changes to html
+        //Apply changes to html
         typingtext.innerHTML = text;
 
         //Add our offset by adding both lengths of our span tags
         pOffset += firstP.length + closingP.length;
         currentindex++;
 
-        alert(currentindex);
+    } else {
+        //The beginning of the red span of text
+        let firstP = "<span style='color:red'>";
+
+        //Construct innerHtml by, [LEFT OF STRING] + <span style='color:red'> + [LETTER] + </span> + [RIGHT OF STRING]
+        text = leftSpan + firstP + letter + closingP + rightSpan;
+
+        //Apply changes to html
+        typingtext.innerHTML = text;
+
+        //Add our offset by adding both lengths of our span tags
+        pOffset += firstP.length + closingP.length;
+        currentindex++;
     }
 };
