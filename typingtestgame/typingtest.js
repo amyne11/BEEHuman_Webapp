586313cc19dc3bf87dc8c6f3c26cf711090249e3
Allen <allen.zhao@student.manchester.ac.uk>
2023-03-02 23:38:17 +0000

    finished typingtestv2's functionality, need to fix a few things on the css styling (combining both of the stylsheets into one), and also need to add comments too

--- typingtestgame/typingtest.js
+++ typingtestgame/typingtest.js
@@ -1,58 +1,96 @@
 let text = "the quick brown fox jumps over the lazy dog";
 let currentindex = 0;
 let pOffset = 0;
+let lastLetter = "";
+let lastKeyCorrect = false;
 
 let typingtext = document.createElement("p");
 typingtext.innerHTML = text;
 
 document.getElementById("typingtext").appendChild(typingtext);
 
-
 document.onkeydown = function (e) {
     e = e || window.event;
 
     if (e.shiftKey || e.ctrlKey) return;
-    
-    
 
     let closingP = "</span>";
 
-    //Find where we are in the inner html taking into account spans we have added
+    // Find where we are in the inner html taking into account spans we have added
     let indexWithOffset = pOffset + currentindex;
 
-    //Find left of the string with regard to our offset index
-    let leftSpan = text.substring(0,indexWithOffset);
-    //Find character we are currently on
+    // Find left of the string with regard to our offset index
+    let leftSpan = text.substring(0, indexWithOffset);
+
+    // Find character we are currently on
     let letter = text[indexWithOffset];
-    //Find right of the string with regard to our offset index
+
+    // Find right of the string with regard to our offset index
     let rightSpan = text.substring(indexWithOffset + 1, text.length);
 
     if (letter === e.key) {
-        //The beginning of the green span of text
+        lastKeyCorrect = true;
+        lastLetter = e.key;
+
+        // The beginning of the green span of text
         let firstP = "<span style='color:green'>";
-        
-        //Construct innerHtml by, [LEFT OF STRING] + <span style='color:green'> + [LETTER] + </span> + [RIGHT OF STRING]
+
+        // Construct innerHtml by, [LEFT OF STRING] + <span style='color:green'> + [LETTER] + </span> + [RIGHT OF STRING]
         text = leftSpan + firstP + letter + closingP + rightSpan;
 
-        //Apply changes to html
+        // Apply changes to html
         typingtext.innerHTML = text;
 
-        //Add our offset by adding both lengths of our span tags
+        // Add our offset by adding both lengths of our span tags
         pOffset += firstP.length + closingP.length;
         currentindex++;
 
+    } else if (e.key === "Backspace") {
+
+        if (currentindex > 0) {
+
+            // If the last key was correct, we need to remove the green span tags
+            if (lastKeyCorrect) {
+                firstP = "<span style='color:green'>";
+            } else {
+                firstP = "<span style='color:red'>";
+            }
+
+            // Make leftSpan = everything before the span tag
+            leftSpan = text.substring(0, indexWithOffset - (firstP.length - 1) - 9);
+
+            // Make rightSpan = everything after the span tag
+            rightSpan = text.substring(indexWithOffset, text.length);
+
+            // Update the text
+            text = leftSpan + lastLetter + rightSpan;
+
+            // Update indexWithOffset
+            indexWithOffset -= firstP.length + closingP.length;
+            currentindex--;
+
+            // Apply changes to html
+            typingtext.innerHTML = text;
+        }
+
     } else {
-        //The beginning of the red span of text
+        lastKeyCorrect = false;
+        lastLetter = text[indexWithOffset];
+
+        // The beginning of the red span of text
         let firstP = "<span style='color:red'>";
 
-        //Construct innerHtml by, [LEFT OF STRING] + <span style='color:red'> + [LETTER] + </span> + [RIGHT OF STRING]
+        // Construct innerHtml by, [LEFT OF STRING] + <span style='color:red'> + [LETTER] + </span> + [RIGHT OF STRING]
         text = leftSpan + firstP + letter + closingP + rightSpan;
 
-        //Apply changes to html
+        // Apply changes to html
         typingtext.innerHTML = text;
 
-        //Add our offset by adding both lengths of our span tags
+        // Add our offset by adding both lengths of our span tags
         pOffset += firstP.length + closingP.length;
         currentindex++;
     }
+
+    // Alert for testing purposes
+    // alert(`current index: ${currentindex}, offset: ${pOffset}, index with offset: ${indexWithOffset}`);
 };
---
+++ typingtestgame/typingtestv2.js
@@ -0,0 +1,151 @@
+const paragraphs = [
+
+    "The quick brown fox jumps over the lazy dog.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resulx tant pleasure?"
+];
+
+// Get the necessary HTML elements
+const typingText = document.querySelector(".typingText p")
+const inpText = document.querySelector(".wrapper .inputText")
+const againBtn = document.querySelector(".content button")
+const timeTag = document.querySelector(".time span b")
+const mistakeTag = document.querySelector(".mistakes span")
+const wpmTag = document.querySelector(".wpm span")
+const wpmRes = document.querySelector(".wpmResult span")
+const mistakesRes = document.querySelector(".mistakesResult span")
+const accuracyRes = document.querySelector(".accuracyResult span")
+
+// Initialize some variables
+let timer;
+let maxTime = 60;
+let timeRem = maxTime;
+let indexOfChar = 0;
+let numOfMistakes = 0;
+let isTyping = 0;
+let finalWPM = 0;
+let finalMistakes = 0;
+let finalAccuracy = 0;
+
+// Load a random paragraph for the user to type
+function loadParagraph() {
+    const randPara = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    // Split the paragraph into individual characters and add each character to the HTML as a <span>
+    paragraphs[randPara].split("").forEach(char => {
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    // Highlight the first character in the paragraph
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    // Set focus to the input field when the user clicks anywhere in the paragraph
+    document.addEventListener("keydown", () => inpText.focus());
+    typingText.addEventListener("click", () => inpText.focus());
+}
+
+// Check if the user's typing matches the expected text
+function Typing() {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpText.value.split("")[indexOfChar];
+    // If there are still characters to type and time has not run out
+    if (indexOfChar < characters.length - 1 && timeRem > 0) {
+        // If the user just started typing, start the timer
+        if (!isTyping) {
+            timer = setInterval(initialiseTimer, 1000);
+            isTyping = true;
+        }
+        // If the user backspaces, delete the previous character
+        if (typedChar == null) {
+            if (indexOfChar > 0) {
+                indexOfChar--;
+                // If the previous character was incorrect, decrement the mistake count
+                if (characters[indexOfChar].classList.contains("incorrect")) {
+                    numOfMistakes--;
+                }
+                // Remove the "correct" and "incorrect" classes from the previous character
+                characters[indexOfChar].classList.remove("correct", "incorrect");
+            }
+        // If the user types a character, check if it is correct
+        } else {
+            if (characters[indexOfChar].innerText == typedChar) {
+                characters[indexOfChar].classList.add("correct");
+            } else {
+                // If the user typed the wrong character, increment the mistake count
+                numOfMistakes++;
+                characters[indexOfChar].classList.add("incorrect");
+            }
+            indexOfChar++;
+        }
+        // Remove the "active" class from all characters and add it to the current character
+        characters.forEach(span => span.classList.remove("active"));
+        characters[indexOfChar].classList.add("active");
+
+        // Update the WPM and mistake count displayed to the user
+        wpmTag.innerText = calcWPM();
+        mistakeTag.innerText = numOfMistakes;
+    // If the user has typed all the characters or time has run out
+    } else {
+        
+        // Calculate the final WPM, mistakes, and accuracy
+        finalWPM = calcWPM();
+        finalMistakes = numOfMistakes;
+        finalAccuracy = Math.round((indexOfChar - numOfMistakes) / indexOfChar * 100);
+        // Display the final WPM, mistakes, and accuracy to the user
+        wpmRes.innerText = finalWPM;
+        mistakesRes.innerText = finalMistakes;
+        accuracyRes.innerText = finalAccuracy;
+        // Stop the timer and clear the input field
+        clearInterval(timer);
+        inpText.value = "";
+    }
+}
+
+// Calculates and returns the user's words per minute (WPM) score
+function calcWPM() {
+    let wpm = Math.round(((indexOfChar - numOfMistakes) / 5) / (maxTime - timeRem) * 60);
+    
+    // Checks if the WPM score is invalid and sets it to zero if it is
+    if (wpm < 0 || !wpm || wpm === Infinity) {
+        wpm = 0;
+    } else {
+        wpm = wpm;
+    }
+    
+    return wpm;
+}
+
+// Initializes and updates the timer and WPM score display
+function initialiseTimer() {
+    // Decrements the time remaining and updates the time display
+    if (timeRem > 0) {
+        timeRem--;
+        timeTag.innerText = timeRem;
+        
+        // Calculates and updates the WPM score display
+        let wpm = Math.round(((indexOfChar - numOfMistakes) / 5) / (maxTime - timeRem) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+// Resets the game and all relevant variables and displays
+function resetGame() {
+    // Loads a new paragraph, clears the timer, and resets all variables
+    loadParagraph();
+    clearInterval(timer);
+    timeRem = maxTime;
+    indexOfChar = numOfMistakes = isTyping = 0;
+    
+    // Resets the input text and all score displays
+    inpText.value = "";
+    timeTag.innerText = timeRem;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+}
+
+// Loads the initial paragraph and sets up event listeners for typing and reset
+loadParagraph();
+inpText.addEventListener("input", Typing);
+againBtn.addEventListener("click", resetGame);
