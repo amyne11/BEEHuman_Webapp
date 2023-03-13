b06a2e14b6c5db3b3faf6df22b776cd7e0f530d8
Oluwaniayomi Ogundana <oluwaniayomi.ogundana@student.manchester.ac.uk>
2023-03-13 14:48:04 +0000

    selectResult and selectResults functions done.

--- backend/models/leaderboards.php
+++ backend/models/leaderboards.php
@@ -18,9 +18,37 @@ class Leaderboards extends Db {
         $stmt->execute([$username, $score]);
     }
 
-    public static function selectResult($game, $username)
+    public function selectResult($game, $username)
     {
+        $this->table = ucfirst($game) . "Table";
+        $this->columnName = $game . "Score";
+
+        $sql = "SELECT {$this->columnName} FROM {$this->table} WHERE userName=?";
         
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$username]);
+        $score = $stmt->fetch();
+        return $score;
+    }
+
+    public function selectResults(){
+        $tables = array("TypingTable", "BarioTable", "ReactionTest");
+        $columns = array("typingScore", "barioScore", "reactionScore");
+        $leaderboardArry = array(); //I don't know if we need to define the size of the array beforehand
+        for ($i=0; $i<count($tables); $i++){
+            $this->table = $tables[$i];
+            $this->columnName = $columns[$i];
+
+            $sql = "SELECT {$this->columnName} FROM {$this->table} ORDER BY {$this->columnName} LIMIT 10";
+            
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            $scores = $stmt->fetch();
+            $leaderboardArry[$i] = $scores;
+        }
+        $leaderboardArry[2] = array_reverse($leaderboardArry[2]); //'2' is the position of the reaction test in the array
+        die(var_dump($leaderboardArry));
+        // return $leaderboardArry;
     }
 }
 
