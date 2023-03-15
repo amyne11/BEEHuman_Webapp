ac6c67b14936986cbc290f998676b1124ca460be
Allen <allen.zhao@student.manchester.ac.uk>
2023-03-15 22:06:11 +0000

    Final touches to typing test

--- typingtestgame/typingtestv2.js
+++ typingtestgame/typingtestv2.js
@@ -3,7 +3,7 @@ const paragraphs = [
     "The quick brown fox jumps over the lazy dog.",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resulx tant pleasure?"
+    "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
 ];
 
 //Get the necessary HTML elements
@@ -29,26 +29,31 @@ let finalMistakes = 0;
 let finalAccuracy = 0;
 
 //Load a random paragraph for the user to type
-function loadParagraph() {
+function loadParagraph() 
+{
     const randPara = Math.floor(Math.random() * paragraphs.length);
     typingText.innerHTML = "";
+    
     //Split the paragraph into individual characters and add each character to the HTML as a <span>
-    paragraphs[randPara].split("").forEach(char => 
-    {
-        let span = `<span>${char}</span>`
-        typingText.innerHTML += span;
-    });
+    const chars = paragraphs[randPara].split("");
+    const spanElements = chars.map(char => { return `<span>${char}</span>`; });
+    typingText.innerHTML = spanElements.join("");
+    
     //Highlight the first character in the paragraph
-    typingText.querySelectorAll("span")[0].classList.add("active");
+    typingText.querySelector("span:first-child").classList.add("active");
+    
     //Set focus to the input field when the user clicks anywhere in the paragraph
     document.addEventListener("keydown", () => inputText.focus());
     typingText.addEventListener("click", () => inputText.focus());
 }
 
+
 //Check if the user's typing matches the expected text
-function Typing() {
-    let characters = typingText.querySelectorAll("span");
-    let typedChar = inputText.value.split("")[currentindex];
+function Typing()
+{
+    const characters = typingText.querySelectorAll("span");
+    const typedChar = inputText.value[currentindex];
+    
     //If there are still characters to type and time has not run out
     if (currentindex < characters.length && timeRem > 0) 
     {
@@ -59,7 +64,7 @@ function Typing() {
             isTyping = true;
         }
         //If the user backspaces, delete the previous character
-        if (typedChar == null) 
+        if (typedChar === undefined) 
         {
             if (currentindex > 0) 
             {
@@ -73,12 +78,13 @@ function Typing() {
                 characters[currentindex].classList.remove("correct", "incorrect");
             }
         } 
-        //If the user types a character, check if it is correct
-        else {
-            if (characters[currentindex].innerText == typedChar) 
+        else 
+        {
+            //If the user types a character, check if it is correct
+            if (characters[currentindex].innerText === typedChar) 
             {
                 characters[currentindex].classList.add("correct");
-            }
+            } 
             else 
             {
                 //If the user typed the wrong character, increment the mistake count
@@ -94,8 +100,9 @@ function Typing() {
         //Update the WPM and mistake count displayed to the user
         wpmTag.innerText = calcWPM();
         mistakeTag.innerText = numOfMistakes;
-    } 
+    }
 }
+
 
 //Calculates and returns the user's words per minute (WPM) score
 function calcWPM() 
@@ -106,7 +113,9 @@ function calcWPM()
     if (wpm < 0 || !wpm || wpm === Infinity) 
     {
         wpm = 0;
-    } else {
+    } 
+    else 
+    {
         wpm = wpm;
     }
     
@@ -139,7 +148,6 @@ function initialiseTimer()
         //Stop the timer and clear the input field
         clearInterval(timer);
         inputText.value = "";
-
     }
 }
 
@@ -150,7 +158,12 @@ function resetGame()
     loadParagraph();
     clearInterval(timer);
     timeRem = maxTime;
-    currentindex = numOfMistakes = isTyping = 0;
+    currentindex = 0;
+    numOfMistakes = 0;
+    isTyping = 0;
+    finalWPM = 0;
+    finalMistakes = 0;
+    finalAccuracy = 0;
     
     //Resets the input text and all score displays
     inputText.value = "";
